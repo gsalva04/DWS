3dd31e8f1c78789ac3f9e5dbee02cc6dffa08604
Gonzalo Salvá <gsalva04@gmail.com>
2023-01-13 19:00:07 +0100

    ...

---
+++ 8_PracticaFormulariosHtml/validar_formulario.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$id = $_POST['id'];
+$genero = $_POST['genero'];
+$edad = $_POST['edad'];
+$observacion = $_POST['observacion'];
+$localidad = $_POST['localidad'];
+$desayuno = $_POST['desayuno'];
+$comida = $_POST['comida'];
+$cena = $_POST['cena'];
+
+
+echo "El nombre es: ".$nombre."<br>";
+echo "El apellido es: ".$apellido."<br>";
+echo "El identificador es: ".$id."<br>";
+echo "El genero es: ".$genero."<br>";
+echo "La edad es: ".$edad."<br>";
+echo "La observación es: ".$observacion."<br>";
+echo "La localidad es: ".$localidad."<br>";
+echo "La valoración del desayuno es: ".$desayuno."<br>";
+echo "La valoración de la comida es: ".$comida."<br>";
+echo "La valoración de la cena es: ".$cena."<br>";
+
+
